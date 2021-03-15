7dcca2490ee7a2a0a91051fe0e99e3dc0b4481e4
obraun <obraun@ssystems.de>
2021-03-15 20:01:46 +0100

    MOD add geo koos for saving votes

--- android/app/src/main/java/com/voxipo/voxpopuli/MainApplication.java
+++ android/app/src/main/java/com/voxipo/voxpopuli/MainApplication.java
@@ -3,6 +3,7 @@ package com.voxipo.voxpopuli;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new GeolocationPackage(),
             new SafeAreaContextPackage(),
             new RNGestureHandlerPackage()
       );
